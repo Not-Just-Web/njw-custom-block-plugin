88a88306dac1a04bdb23a2b737265e19c848704f
Sanjay Adhikari <culblank.minded@gmail.com>
2024-05-02 09:20:18 +1000

    Building the block

--- build/blocks/njw-product-cat-cover-image/index.asset.php
+++ build/blocks/njw-product-cat-cover-image/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-server-side-render'), 'version' => '2f23e3f9f16277964b09');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-server-side-render'), 'version' => 'c348b30d7f1719fba879');
