6eff594e1830180a45ec15d3ec6a77b9e2b6124e
Sanjay Adhikari <culblank.minded@gmail.com>
2024-05-03 09:40:18 +1000

    Retaining cover design and css

--- build/blocks/njw-product-cat-cover-image/index.asset.php
+++ build/blocks/njw-product-cat-cover-image/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-server-side-render'), 'version' => '861c7326e285d08c6e36');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-server-side-render'), 'version' => '8669861d2d51c0ae8d51');
