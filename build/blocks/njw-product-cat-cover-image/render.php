fcdbdbdc2cc6e7d10b64a9f207f557ee53ea5dd1
Sanjay Adhikari <culblank.minded@gmail.com>
2024-05-03 13:38:24 +1000

    Minor change fix

--- build/blocks/njw-product-cat-cover-image/render.php
+++ build/blocks/njw-product-cat-cover-image/render.php
@@ -15,10 +15,9 @@ $category_banner_url = wp_get_attachment_url($category_banner_id);
 
 ob_start(); // Start output buffering
 ?>
-	<!-- wp:cover {"overlayColor":"lite","minHeight":450,"isDark":false} -->
+	<!-- wp:cover {"overlayColor":"lite", "url":"<?php echo esc_url($category_banner_url); ?>", "minHeight":450,"isDark":false} -->
 	<div class="wp-block-cover is-light" style="min-height:450px"><span aria-hidden="true"
 			class="wp-block-cover__background has-lite-background-color has-background-dim-100 has-background-dim"></span>
-		<!-- wp:cover {"url":"<?php echo esc_url($category_banner_url); ?>","overlayColor":"lite","minHeight":450,"isDark":false} -->
 		<div class="wp-block-cover__inner-container">
 			<!-- wp:group {"style":{"spacing":{"blockGap":"40px"}},"layout":{"inherit":true}} -->
 			<div class="wp-block-group">
